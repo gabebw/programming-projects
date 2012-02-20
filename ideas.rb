4dcba811547daf5a2198b3c9b4d7f33d8e7869c1
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-20 18:43:40 -0500

    Style a bit.

--- ideas.rb
+++ ideas.rb
@@ -3,5 +3,5 @@ IDEAS = [
   "Yuppie baby name generator",
   "Yuppie baby name generator...IN THE CLOUD",
   "A service that adds text to arbitrary images. Hint: http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN",
-  "A human year <-> dog year calculator"
+  "A human year &harr; dog year calculator"
 ]
