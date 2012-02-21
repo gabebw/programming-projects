e820d2c00fe792c638b41e0eb4f169d2379a15eb
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-21 07:41:00 -0500

    Make ideas read better when on the page.

--- lib/ideas.rb
+++ lib/ideas.rb
@@ -1,7 +1,7 @@
 IDEAS = [
   "This web app. Yes, the one you're using right now.",
-  "Yuppie baby name generator",
-  "Yuppie baby name generator...IN THE CLOUD",
+  "A yuppie baby name generator",
+  "A yuppie baby name generator...IN THE CLOUD",
   %{A service that adds any text to any images. Like this (dynamically-created!) image:<br/> <img src="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN" />},
   "A human year &harr; dog year calculator"
 ]
