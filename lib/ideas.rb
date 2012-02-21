c983ee8e9bfda9eb76cd9597df78b768181dd0b5
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-21 08:05:54 -0500

    The image was too big. Link to it instead.

--- lib/ideas.rb
+++ lib/ideas.rb
@@ -2,6 +2,6 @@ IDEAS = [
   "This web app. Yes, the one you're using right now.",
   "A yuppie baby name generator",
   "A yuppie baby name generator...IN THE CLOUD",
-  %{A service that adds any text to any images. Like this (dynamically-created!) image:<br/> <img src="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN" />},
+  %{A service that adds any text to any images. Like this image <a href="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN">right here</a>.},
   "A human year &harr; dog year calculator"
 ]
