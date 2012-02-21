87b3aaed6cc9fcfaa38b208484f1d8d9e28da55a
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-21 18:15:02 -0500

    Add a couple more ideas.

--- lib/ideas.rb
+++ lib/ideas.rb
@@ -1,7 +1,11 @@
 IDEAS = [
   "This web app. Yes, the one you're using right now.",
   "A yuppie baby name generator",
-  "A yuppie baby name generator...IN THE CLOUD",
+  "A yuppie baby name generator ... IN THE CLOUD",
   %{A service that adds any text to any image. Like this image <a href="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN">right here</a>.},
-  "A human year &harr; dog year calculator"
+  "A human year &harr; dog year calculator",
+  "A to-do list",
+  "A to-do list ... IN THE CLOUD",
+  "Mad Libs!",
+  "A choose-your-own adventure game"
 ]
