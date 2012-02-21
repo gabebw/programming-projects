26c49279fcbb49f79582db04e6162b47877e0049
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-21 08:12:51 -0500

    Add permalink so users can bookmark an idea.

--- lib/app.rb
+++ lib/app.rb
@@ -4,9 +4,17 @@ require_relative 'ideas'
 set :root, File.expand_path("../..", __FILE__)
 
 get '/' do
-  idea = IDEAS.sample
+  idea  = IDEAS.sample
+  index = IDEAS.index(idea)
   erb :index, layout: :layout,
-              locals: { idea: idea }
+              locals: { idea: idea, index: index }
+end
+
+get '/:index' do
+  index = params[:index].to_i
+  idea  = IDEAS[index]
+  erb :index, layout: :layout,
+              locals: { idea: idea, index: index }
 end
 
 get "/css/:name.css" do
