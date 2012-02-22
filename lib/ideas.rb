653a81a153d841cf841ab906bb073f49d575fcf5
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-21 19:28:04 -0500

    Let's use single quotes.

--- lib/ideas.rb
+++ lib/ideas.rb
@@ -2,7 +2,7 @@ IDEAS = [
   "This web app. Yes, the one you're using right now.",
   "A yuppie baby name generator",
   "A yuppie baby name generator ... IN THE CLOUD",
-  %{A service that adds any text to any image. Like this image <a href="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN">right here</a>.},
+  %q{A service that adds any text to any image. Like this image <a href="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN">right here</a>.},
   "A human year &harr; dog year calculator",
   "A to-do list",
   "A to-do list ... IN THE CLOUD",
