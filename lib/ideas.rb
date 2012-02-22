ea3b3ea591d42f428925a38cf026f7e9976398fa
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-21 19:44:02 -0500

    Add some ideas, comment out ideas that are too hard and thus discouraging.

--- lib/ideas.rb
+++ lib/ideas.rb
@@ -1,9 +1,5 @@
 IDEAS = [
-  # I like this one. Let's increase its probability.
   "This web app. Yes, the one you're using right now.",
-  "This web app. Yes, the one you're using right now.",
-  "This web app. Yes, the one you're using right now.",
-
   "A yuppie baby name generator",
   "A yuppie baby name generator ... IN THE CLOUD",
   %q{A service that adds any text to any image. Like this image <a href="http://memecaptain.com/i?u=http://images5.fanpop.com/image/photos/24800000/Hufflepuff-backpack-lifesgoodx3-24857969-500-500.jpg&tt=ERRYDAY+I'M&tb=PUFFLIN">right here</a>.},
@@ -13,7 +9,14 @@ IDEAS = [
   "Mad Libs!",
   "A choose-your-own adventure game",
   "A guess-what-number-I'm-thinking-of game",
-  "A service that tells you if that's what she said. It's not that hard. (That's what she said.)",
-  "An anagram generator.",
-  %q{A little app to manipulate photos, <a href="http://camanjs.com/#example-table">like this</a>. You can build something awesome for <a href="http://www.photohackday.org/about.html">Photo Hack Day</a>!}
+  # "A service that tells you if that's what she said. It's not that hard. (That's what she said.)",
+  # "An anagram generator.",
+  # %q{A little app to manipulate photos, <a href="http://camanjs.com/#example-table">like this</a>. You can build something awesome for <a href="http://www.photohackday.org/about.html">Photo Hack Day</a>!}
+  "Ask for two numbers and add them",
+  "Ask for two numbers and subtract them",
+  "Ask for the lengths of two sides of a right triangle and display the length of the hypotenuse",
+  "A coin-toss program (heads or tails?)",
+  "Ask for your first and last name and display your full name",
+  "Rock paper scissors",
+  "A dice roller"
 ]
