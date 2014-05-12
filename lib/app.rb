3f29adecaebefa2490230402ad31b46ba0d508cb
Gabe Berke-Williams <gabebw@gabebw.com>
2014-05-12 17:10:39 -0400

    Remove footer and permalinks

--- lib/app.rb
+++ lib/app.rb
@@ -5,16 +5,7 @@ set :root, File.expand_path("../..", __FILE__)
 
 get '/' do
   idea  = IDEAS.sample
-  index = IDEAS.index(idea)
-  erb :index, layout: :layout,
-              locals: { idea: idea, index: index }
-end
-
-get '/:idea_index' do
-  index = params[:idea_index].to_i
-  idea  = IDEAS[index]
-  erb :show, layout: :layout,
-             locals: { idea: idea }
+  erb :index, layout: :layout, locals: { idea: idea}
 end
 
 get "/css/:name.css" do
