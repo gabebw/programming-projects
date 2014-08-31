ac0388fc00b18235ae0f0f5743079a01f1063014
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-31 09:24:55 -0400

    Use more idiomatic Sinatra
    * Use ivar instead of a local
    * Use default layout automatically

--- lib/app.rb
+++ lib/app.rb
@@ -7,8 +7,8 @@ require_relative 'ideas'
 set :root, File.expand_path("../..", __FILE__)
 
 get '/' do
-  idea  = IDEAS.sample
-  erb :index, layout: :layout, locals: { idea: idea}
+  @idea  = IDEAS.sample
+  erb :index
 end
 
 get "/css/:name.css" do
